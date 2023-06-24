dcd721bd8541dabf2df2c80c1864842fbd1c20cb
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-06-24 14:17:25 +0530

    Add files via upload

---
+++ lecture5_1.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     int a[5];
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    cout<<"enter 5 integers"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"the integers are: "<<a<<b<<c<<d<<e;
+    return 0;
+
+}
